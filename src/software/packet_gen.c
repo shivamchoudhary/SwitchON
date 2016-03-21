e0ce2e3ce55895cc93deb027e8c6c2716f3c8eda
shivamchoudhary <sc3973@columbia.edu>
2016-03-20 23:55:26 -0400

    Adding Random Number generator and some readme changes

--- src/software/packet_gen.c
+++ src/software/packet_gen.c
@@ -1,8 +1,10 @@
-#include "packet_gen.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <time.h>
+#include "packet_gen.h"
 
 void makePacket(int srcip,int dstip,int srcport, int dstport){
         struct ipheader ipheader_t;
@@ -12,7 +14,9 @@ void makePacket(int srcip,int dstip,int srcport, int dstport){
         ipheader_t.dstport = dstport;
 }
 
-
 int main(int argc, char *argv[]){
+        for (int i=0;i<NUM_PACKETS;i++){
+                printf("%d\n",rand()%65536);
+        }
         return 0;
 }
--- src/software/packet_gen.h
+++ src/software/packet_gen.h
@@ -1,14 +1,9 @@
 #define PACKET_LEN 8192
+#define NUM_PACKETS 2
 struct ipheader {
- unsigned char      iph_ihl:5, iph_ver:4;
- unsigned char      iph_tos;
- unsigned short int iph_len;
- unsigned short int iph_ident;
- unsigned char      iph_flag;
- unsigned short int iph_offset;
- unsigned char      iph_ttl;
- unsigned char      iph_protocol;
- unsigned short int iph_chksum;
- unsigned int       iph_sourceip;
- unsigned int       iph_destip;
+ unsigned short int chksum;
+ unsigned int       src;
+ unsigned int       dst;
+ unsigned int       srcport;
+ unsigned int       dstport;
 };
