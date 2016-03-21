bde4f4c0de161f958ae3d3fe4aabdca6098c8a30
shivamchoudhary <sc3973@columbia.edu>
2016-03-20 23:37:39 -0400

    Adding Makefiles with .PHONY

--- src/software/packet_gen.c
+++ src/software/packet_gen.c
@@ -1,9 +1,18 @@
 #include "packet_gen.h"
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+void makePacket(int srcip,int dstip,int srcport, int dstport){
+        struct ipheader ipheader_t;
+        ipheader_t.src = srcip;
+        ipheader_t.dst = dstip;
+        ipheader_t.srcport = srcport;
+        ipheader_t.dstport = dstport;
+}
 
 
-
-int main(int argc, int *argv[]){
-        a = ipheader()
+int main(int argc, char *argv[]){
         return 0;
 }
