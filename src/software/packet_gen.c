078313f3b0c8cd37c31b7c85fe40e9123b1d185f
shivamchoudhary <sc3973@columbia.edu>
2016-03-24 16:18:55 -0400

    Removing Superflous code

--- src/software/packet_gen.c
+++ src/software/packet_gen.c
@@ -8,27 +8,10 @@
 #include "packet_gen.h"
 #include "common.h"
 
-void makePacket(int srcip,int dstip,int srcport, int dstport){
-        struct ipheader ipheader_t;
-        ipheader_t.src = srcip;
-        ipheader_t.dst = dstip;
-        ipheader_t.srcport = srcport;
-        ipheader_t.dstport = dstport;
-}
-
 int main(int argc, char *argv[]){
-        
         time_t t;
         srand((unsigned) time(&t));
         for (int i=START_PORT; i<START_PORT+NUM_PACKETS; i++){
-                char fname[100];
-                sprintf(fname,"../../output/%d.pkt",i);
-                FILE *fd;
-                fd =fopen(fname,"w+");
-                if (fd==NULL){
-                        printf("Failed to open file %s",fname);
-                }
-                fprintf(fd,"TEST\n");
                 binary(rand()%3);
         
         }
--- src/software/packet_gen.h
+++ src/software/packet_gen.h
@@ -1,10 +1,9 @@
 #define PACKET_LEN 8192
 #define START_PORT 4840
 #define NUM_PACKETS 20
-struct ipheader {
- unsigned short int chksum;
- unsigned int       src;
- unsigned int       dst;
- unsigned int       srcport;
- unsigned int       dstport;
-};
+
+void convert2binary(int num);
+
+
+
+
